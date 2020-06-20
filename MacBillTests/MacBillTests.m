d84a90ae40ac67635d9fc378c80d626be08d6a27
Alistair McMillan <alistairmcmillan75@gmail.com>
2020-06-20 22:38:36 +0100

    Travis doesn't seem to support UI tests

---
+++ MacBillTests/MacBillTests.m
@@ -0,0 +1,27 @@
+//
+//  MacBillTests.m
+//  MacBillTests
+//
+//  Created by Alistair McMillan on 20/06/2020.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MacBillTests : XCTestCase
+@end
+
+@implementation MacBillTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testWritingToScorelist {
+    // Put tests here
+}
+
+@end
