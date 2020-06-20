5265da0493a7a29eb1e348b4ac88a480e189d483
Alistair McMillan <alistairmcmillan75@gmail.com>
2020-06-20 20:00:33 +0100

    Travis doesn't like XCTOSSignpostMetric

--- MacBillUITests/MacBillUITests.m
+++ MacBillUITests/MacBillUITests.m
@@ -35,13 +35,4 @@
     // Use XCTAssert and related functions to verify your tests produce the correct results.
 }
 
-- (void)testLaunchPerformance {
-    if (@available(macOS 10.15, iOS 13.0, tvOS 13.0, *)) {
-        // This measures how long it takes to launch your application.
-        [self measureWithMetrics:@[XCTOSSignpostMetric.applicationLaunchMetric] block:^{
-            [[[XCUIApplication alloc] init] launch];
-        }];
-    }
-}
-
 @end
